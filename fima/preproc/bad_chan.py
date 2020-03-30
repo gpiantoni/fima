13940e494019e7c7d7295b17255f1e08c3e915e4
Gio on ArchXPS <github@gpiantoni.com>
2020-03-30 08:37:35 +0200

    use another channel to test reference

--- fima/preproc/bad_chan.py
+++ fima/preproc/bad_chan.py
@@ -2,7 +2,7 @@ from wonambi.trans import montage, concatenate, math
 
 
 def find_good_channels(data):
-    data = montage(data, ref_chan=list(data.chan[0][:1]))  # reference to first channel
+    data = montage(data, ref_chan=list(data.chan[0][1:2]))  # reference to first channel
     data = concatenate(data, axis='time')
 
     data = math(data, operator_name='std', axis='time')
