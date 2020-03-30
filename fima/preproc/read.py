e93eaac2afacc2d3a83eb8294741ef30d0aa3810
Gio on ArchXPS <github@gpiantoni.com>
2020-03-30 11:48:40 +0200

    select events

--- fima/preproc/read.py
+++ fima/preproc/read.py
@@ -5,10 +5,44 @@ from numpy import array
 from .bad_chan import find_good_channels
 
 
-def read_data(filename):
+FINGERS_OPEN = [
+    'thumb open',
+    'index open',
+    'middle open',
+    'ring open',
+    'little open',
+    ]
+
+
+FINGERS_CLOSED = [
+    'thumb close',
+    'index close',
+    'middle close',
+    'ring close',
+    'little close',
+    ]
+
+
+FINGERS = {
+    'thumb': ['thumb open', 'thumb close'],
+    'index': ['index open', 'index close'],
+    'middle': ['middle open', 'middle close'],
+    'ring': ['ring open', 'ring close'],
+    'little': ['little open', 'little close'],
+    }
+
+
+def read_data(filename, event='open'):
+
+    if event == 'open':
+        event_list = FINGERS_OPEN
+    elif event == 'closed':
+        event_list = FINGERS_CLOSED
+    else:
+        event_list = FINGERS[event]
+
     d = Dataset(filename, bids=True)
-    mrk = d.read_markers()
-    events = [x['start'] for x in mrk if 'open' in x['name']]
+    events = select_events(d, event_list)
 
     is_ecog = d.dataset.task.channels.tsv['type'] == 'ECOG'
     is_seeg = d.dataset.task.channels.tsv['type'] == 'SEEG'
@@ -21,3 +55,13 @@ def read_data(filename):
     data = montage(data, ref_to_avg=True)
 
     return data
+
+
+def select_events(d, events):
+
+    onsets = []
+    for evt in d.read_markers():
+        for event_name in events:
+            if evt['name'].startswith(event_name):
+                onsets.append(evt['start'])
+    return onsets
