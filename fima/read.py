a2f387e3529b125f8d6836f3f89e0ebcc1754647
Gio on ArchXPS <github@gpiantoni.com>
2020-04-16 12:07:16 +0200

    use bids format for finger movements

--- fima/read.py
+++ fima/read.py
@@ -11,6 +11,7 @@ def load(what, subject, run=None, acq=None):
       - 'data' returns: ChanTime, event_names
       - 'events' returns: ndarray
       - 'dataglove' returns: ndarray
+      - 'movements' returns: ndarray
 
     """
     if run is None:
