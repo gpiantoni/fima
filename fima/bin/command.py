892019b1f069a07bf865c09aa3d69eb9280510a2
Gio on ArchXPS <github@gpiantoni.com>
2020-04-24 17:44:19 +0200

    command for fitting

--- fima/bin/command.py
+++ fima/bin/command.py
@@ -2,10 +2,12 @@
 
 from ..pipelines.power_spectrum import pipeline_timefreq_all
 from ..pipelines.fingers import pipeline_fingers_all
+from ..pipelines.fitting import pipeline_fitting_all
 
 
 def main():
-    pipeline_fingers_all()
+    # pipeline_fingers_all()
+    pipeline_fitting_all()
 
 
 if __name__ == '__main__':
