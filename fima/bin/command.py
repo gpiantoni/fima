6fe2666a554c201834e679f5a0058b8f2d683b11
Gio at UMCU <github@gpiantoni.com>
2020-04-29 17:34:58 +0200

    run all models

--- fima/bin/command.py
+++ fima/bin/command.py
@@ -6,8 +6,7 @@ from ..pipelines.fitting import pipeline_fitting_all
 
 
 def main():
-    # pipeline_fingers_all()
-    pipeline_fitting_all('linear_separate_gaussians_per_finger')
+    pipeline_fingers_all()
 
 
 if __name__ == '__main__':
